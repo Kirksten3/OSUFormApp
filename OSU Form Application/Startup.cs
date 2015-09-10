ef1ab9c0dedcf6de18d02d8ef136a9745e743249
Kirk <kjsstennett@gmail.com>
2015-09-09 21:24:15 -0700

    Initial File Commit

---
+++ OSU Form Application/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(OSU_Form_Application.Startup))]
+namespace OSU_Form_Application
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
